1ce56774e878dfe468bf6f16e242044409c3d7bd
Justin Case <hello@justincase.cx>
2014-12-20 00:09:50 +0100

    use spaces

--- lib/Mollie/API/Object/Method.rb
+++ lib/Mollie/API/Object/Method.rb
@@ -1,29 +1,29 @@
 module Mollie
-	module API
-		module Object
-			class Method < Base
-				IDEAL        = "ideal"
-				CREDITCARD   = "creditcard"
-				MISTERCASH   = "mistercash"
-				BANKTRANSFER = "banktransfer"
-				PAYPAL       = "paypal"
-				PAYSAFECARD  = "paysafecard"
-				BITCOIN      = "bitcoin"
-				SOFORT       = "sofort"
+  module API
+    module Object
+      class Method < Base
+        IDEAL        = "ideal"
+        CREDITCARD   = "creditcard"
+        MISTERCASH   = "mistercash"
+        BANKTRANSFER = "banktransfer"
+        PAYPAL       = "paypal"
+        PAYSAFECARD  = "paysafecard"
+        BITCOIN      = "bitcoin"
+        SOFORT       = "sofort"
 
-				attr_accessor :id,
-				              :description,
-				              :amount,
-				              :image
+        attr_accessor :id,
+                      :description,
+                      :amount,
+                      :image
 
-				def getMinimumAmount
-					@amount.minimum
-				end
+        def getMinimumAmount
+          @amount.minimum
+        end
 
-				def getMaximumAmount
-					@amount.maximum
-				end
-			end
-		end
-	end
+        def getMaximumAmount
+          @amount.maximum
+        end
+      end
+    end
+  end
 end
--- lib/Mollie/API/Object/Payment.rb
+++ lib/Mollie/API/Object/Payment.rb
@@ -1,39 +1,39 @@
 module Mollie
-	module API
-		module Object
-			class Payment < Base
-				STATUS_OPEN      = "open"
-				STATUS_CANCELLED = "cancelled"
-				STATUS_EXPIRED   = "expired"
-				STATUS_PAID      = "paid"
-				STATUS_PAIDOUT   = "paidout"
+  module API
+    module Object
+      class Payment < Base
+        STATUS_OPEN      = "open"
+        STATUS_CANCELLED = "cancelled"
+        STATUS_EXPIRED   = "expired"
+        STATUS_PAID      = "paid"
+        STATUS_PAIDOUT   = "paidout"
 
-				attr_accessor :id,
-				              :status,
-				              :mode,
-				              :amount,
-				              :description,
-				              :method,
-				              :createdDatetime,
-				              :paidDatetime,
-				              :expiredDatetime,
-				              :cancelledDatetime,
-				              :metadata,
-				              :details,
-				              :links
+        attr_accessor :id,
+                      :status,
+                      :mode,
+                      :amount,
+                      :description,
+                      :method,
+                      :createdDatetime,
+                      :paidDatetime,
+                      :expiredDatetime,
+                      :cancelledDatetime,
+                      :metadata,
+                      :details,
+                      :links
 
-				def open?
-					@status == STATUS_OPEN
-				end
+        def open?
+          @status == STATUS_OPEN
+        end
 
-				def paid?
-					!@paidDatetime.nil?
-				end
+        def paid?
+          !@paidDatetime.nil?
+        end
 
-				def getPaymentUrl
-					@links && @links.paymentUrl
-				end
-			end
-		end
-	end
+        def getPaymentUrl
+          @links && @links.paymentUrl
+        end
+      end
+    end
+  end
 end

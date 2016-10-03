8a62025ade9f22b7f315e828eb35394992ac1445
Daanvm <daan@mollie.com>
2016-10-03 20:58:05 +0000

    Set up testing environment

---
+++ test/test-empty.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+
+module Test::Unit
+  class TestEmpty < TestCase
+    def test_success
+      assert_true(true)
+    end
+
+#    def test_failure
+#      assert_true(false)
+#    end
+  end
+end

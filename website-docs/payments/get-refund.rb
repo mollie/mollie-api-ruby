2d496c3adda3b84b75cb985be74bac5cb95655b3
Justin Case <hello@justincase.cx>
2018-06-14 23:26:24 +0200

    Fix whitespace

--- website-docs/payments/get-refund.rb
+++ website-docs/payments/get-refund.rb
@@ -2,9 +2,9 @@ require 'mollie-api-ruby'
 
 begin
   refund = Mollie::Payment::Refund.get(
-      "re_4qqhO89gsT",
-      payment_id: 'tr_7UhSN1zuXS',
-      api_key: 'test_dHar4XY7LxsDOtmnkVtjNVWXLSlXsM'
+    "re_4qqhO89gsT",
+    payment_id: 'tr_7UhSN1zuXS',
+    api_key: 'test_dHar4XY7LxsDOtmnkVtjNVWXLSlXsM'
   )
 rescue Mollie::Exception => e
   puts 'An error has occurred: ' << e.message

6827e661ca73e8bbf080308f442d8bb26c38a125
Justin Case <hello@justincase.cx>
2018-07-01 22:30:22 +0200

    Method API: add amount filter example

--- website-docs/methods/list.rb
+++ website-docs/methods/list.rb
@@ -2,6 +2,7 @@ require 'mollie-api-ruby'
 
 begin
   methods = Mollie::Method.all(
+    amount: { value: '100.00', currency: 'EUR' },
     api_key: 'test_dHar4XY7LxsDOtmnkVtjNVWXLSlXsM'
   )
 rescue Mollie::Exception => e

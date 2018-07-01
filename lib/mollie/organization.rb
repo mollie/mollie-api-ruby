ba0ddb7a58593b0a4c6884f0066e01cff1541f52
Justin Case <hello@justincase.cx>
2018-07-01 21:37:46 +0200

    Organizations V2 API Changes
    * postal_code, city, and country_code fields were
      moved to the address object
    * verified_at field has been removed

--- lib/mollie/organization.rb
+++ lib/mollie/organization.rb
@@ -4,16 +4,8 @@ module Mollie
                   :name,
                   :email,
                   :address,
-                  :postal_code,
-                  :city,
-                  :country_code,
                   :registration_number,
-                  :vat_number,
-                  :verified_at
-
-    def verified_at=(verified_at)
-      @verified_at = Time.parse(verified_at.to_s) rescue nil
-    end
+                  :vat_number
 
     def address=(address)
       @address = OpenStruct.new(address) if address.is_a?(Hash)
--- test/mollie/organization_test.rb
+++ test/mollie/organization_test.rb
@@ -7,14 +7,14 @@ module Mollie
         id:                  'org_1234567',
         name:                'Mollie B.V.',
         email:               'info@mollie.com',
-        address:             { "street_and_number"  => 'Keizersgracht 313' },
-        postal_code:         '1016EE',
-        city:                'Amsterdam',
-        country:             'Netherlands',
-        country_code:        'NL',
+        address: {
+          street_and_number: "Keizersgracht 313",
+          postal_code:       "1016 EE",
+          city:              "Amsterdam",
+          country:           "NL"
+        },
         registration_number: '30204462',
         vat_number:          'NL815839091B01',
-        verified_at:         '2007-06-29T09:41:00.0Z'
       }
 
       organization = Organization.new(attributes)
@@ -23,16 +23,11 @@ module Mollie
       assert_equal 'Mollie B.V.', organization.name
       assert_equal 'info@mollie.com', organization.email
       assert_equal 'Keizersgracht 313', organization.address.street_and_number
-      assert_equal '1016EE', organization.postal_code
-      assert_equal 'Amsterdam', organization.city
-      assert_equal 'NL', organization.country_code
+      assert_equal '1016 EE', organization.address.postal_code
+      assert_equal 'Amsterdam', organization.address.city
+      assert_equal 'NL', organization.address.country
       assert_equal '30204462', organization.registration_number
       assert_equal 'NL815839091B01', organization.vat_number
-      assert_equal Time.parse('2007-06-29T09:41:00.0Z'), organization.verified_at
-    end
-
-    def test_verified_at_optional
-      assert_equal nil, Organization.new(verified_at: nil).verified_at
     end
   end
 end

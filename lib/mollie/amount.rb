99cdf73f7cd354fb4c9fe0a7986bcd51fae1f6cc
Justin Case <hello@justincase.cx>
2018-07-02 02:17:29 +0200

    Return empty Amount object when API returns null

--- lib/mollie/amount.rb
+++ lib/mollie/amount.rb
@@ -2,6 +2,10 @@ module Mollie
   class Amount < Base
     attr_accessor :value, :currency
 
+    def initialize(attributes)
+      super unless attributes.nil?
+    end
+
     def value=(net)
       @value = BigDecimal.new(net.to_s)
     end

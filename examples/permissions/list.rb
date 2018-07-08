57a3e0819a4f885c336f362c0cec3cffdb5459a1
Justin Case <hello@justincase.cx>
2018-07-08 17:14:04 +0200

    Update permission examples

--- examples/permissions/list.rb
+++ examples/permissions/list.rb
@@ -1,11 +1,1 @@
-require 'mollie-api-ruby'
-
-begin
-  limit       = 50
-  permissions = Mollie::Permission.all(
-    limit: limit,
-    api_key: 'access_Wwvu7egPcJLLJ9Kb7J632x8wJ2zMeJ'
-  )
-rescue Mollie::Exception => e
-  puts 'An error has occurred: ' << e.message
-end
+permissions = Mollie::Permission.all

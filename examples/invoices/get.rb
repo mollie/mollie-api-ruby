56171540b33ca7340581a108027d306baaec29c0
Justin Case <hello@justincase.cx>
2018-07-08 19:00:24 +0200

    Update invoice examples

--- examples/invoices/get.rb
+++ examples/invoices/get.rb
@@ -1,10 +1,1 @@
-require 'mollie-api-ruby'
-
-begin
-  invoice = Mollie::Invoice.get(
-    "inv_xBEbP9rvAq",
-    api_key: 'test_dHar4XY7LxsDOtmnkVtjNVWXLSlXsM'
-  )
-rescue Mollie::Exception => e
-  puts 'An error has occurred: ' << e.message
-end
+invoice = Mollie::Invoice.get("inv_xBEbP9rvAq")
--- examples/invoices/list.rb
+++ examples/invoices/list.rb
@@ -1,11 +1,1 @@
-require 'mollie-api-ruby'
-
-begin
-  limit     = 50
-  invoices = Mollie::Invoice.all(
-    limit: limit,
-    api_key: 'test_dHar4XY7LxsDOtmnkVtjNVWXLSlXsM'
-  )
-rescue Mollie::Exception => e
-  puts 'An error has occurred: ' << e.message
-end
+invoices = Mollie::Invoice.all

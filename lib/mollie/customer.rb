6a48bebea40665116aa0830446b441f72bbbb67e
Justin Case <hello@justincase.cx>
2018-07-15 00:59:30 +0200

    Sort by method name

--- lib/mollie/customer.rb
+++ lib/mollie/customer.rb
@@ -16,12 +16,12 @@ module Mollie
       @metadata = OpenStruct.new(metadata) if metadata.is_a?(Hash)
     end
 
-    def payments
-      Relation.new(self, Customer::Payment)
-    end
-
     def mandates
       Relation.new(self, Customer::Mandate)
+    end
+
+    def payments
+      Relation.new(self, Customer::Payment)
     end
 
     def subscriptions

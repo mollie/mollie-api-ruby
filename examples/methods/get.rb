80bf9c5e4d54fa0e4f2d4f74da9d5b3cdb03e320
Justin Case <hello@justincase.cx>
2020-01-26 23:39:34 +0100

    Method: add `include=pricing` example

--- examples/methods/get.rb
+++ examples/methods/get.rb
@@ -1,4 +1,11 @@
 method = Mollie::Method.get('ideal')
 
-# Include iDEAL issuers
+# Include issuers available for the payment method (e.g. for
+# iDEAL, KBC/CBC payment button or gift cards).
 method = Mollie::Method.get('ideal', include: 'issuers')
+
+# Include pricing for each payment method
+method = Mollie::Method.get('ideal', include: 'pricing')
+
+# Include both issuers and pricing
+method = Mollie::Method.get('ideal', include: 'issuers,pricing')

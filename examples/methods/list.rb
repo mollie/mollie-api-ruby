ca5935d1700485dff9623e903ebdf7ed587ac27e
Justin Case <hello@justincase.cx>
2021-06-13 12:37:49 +0200

    Add example to retrieve all available payment methods

--- examples/methods/list.rb
+++ examples/methods/list.rb
@@ -2,3 +2,7 @@ methods = Mollie::Method.all
 
 # Filter by amount and currency
 methods = Mollie::Method.all(amount: { value: '100.00', currency: 'EUR' })
+
+# Retrieve all payment methods that Mollie offers and can be activated by
+# the Organization.
+methods = Mollie::Method.all_available

59a28bcf16d42a4a97d3bf2eb6a061cac49bbca8
Justin Case <hello@justincase.cx>
2025-05-10 22:43:42 +0200

    Remove pricing information from Methods API examples
    "We have removed support for the include=pricing query parameter on some
    endpoints of the Methods API. Listing the payment methods with GET /methods or
    retrieving a single method via GET /methods/{id} will no longer return pricing
    information.
    
    If you require pricing information, you can still use the query parameter for
    GET /methods/all."
    
    https://docs.mollie.com/changelog/pricing-information-removed-from-methods-api-endpoints

--- examples/methods/get.rb
+++ examples/methods/get.rb
@@ -1,11 +1,4 @@
 method = Mollie::Method.get('ideal')
 
-# Include issuers available for the payment method (e.g. for
-# iDEAL, KBC/CBC payment button or gift cards).
+# Include issuer details such as which iDEAL or gift card issuers are available
 method = Mollie::Method.get('ideal', include: 'issuers')
-
-# Include pricing for each payment method
-method = Mollie::Method.get('ideal', include: 'pricing')
-
-# Include both issuers and pricing
-method = Mollie::Method.get('ideal', include: 'issuers,pricing')
--- examples/methods/list.rb
+++ examples/methods/list.rb
@@ -1,8 +1,17 @@
+# Retrieve all enabled payment methods
 methods = Mollie::Method.all
 
 # Filter by amount and currency
 methods = Mollie::Method.all(amount: { value: '100.00', currency: 'EUR' })
 
-# Retrieve all payment methods that Mollie offers and can be activated by
-# the Organization.
+# Retrieve all payment methods that Mollie offers and can be activated by the Organization
 methods = Mollie::Method.all_available
+
+# Include issuer details such as which iDEAL or gift card issuers are available
+methods = Mollie::Method.all_available(include: "issuers")
+
+# Include pricing for each payment method
+methods = Mollie::Method.all_available(include: "pricing")
+
+# Include issuer details and pricing
+methods = Mollie::Method.all_available(include: "issuers,pricing")
